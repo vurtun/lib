35e63152937014627bc51e36ad28c8ff6c5a69b8
vurtun <polygone@gmx.net>
2015-12-08 16:29:43 +0100

    uploaded correct preproc test file

--- tests/preproc/test.c
+++ tests/preproc/test.c
@@ -5,50 +5,64 @@
 
 #include "meta.h"
 
-introspect struct rectangle {
-    float x, y, w, h;
+meta_introspect struct test {
+    char c; short s;
+    int i; long l;
+    int *pi; long al[16];
 };
-
-introspect struct test {
-    struct rectangle rect;
-    char c;
-    short s;
-    int i;
-    long l;
-    int *pi;
-    long al[16];
-};
-
-introspect enum state {
+meta_introspect enum state {
     STATE_WAITING,
     STATE_RUNNING,
     STATE_HALTED
 };
-
-introspect enum string_enum {
-    PUNCT_STAR as meta("x"),
+meta_introspect enum punctuation {
+    PUNCT_STAR as meta("*"),
     PUNCT_COMMA as meta(","),
     PUNCT_PLUS as meta("+")
 };
-
-#define introspect_struct(x)
-introspect_struct(int id; const char *name;);
-introspect enum complex_enum {
-
-}
-
-introspect int
-function(int i, char c, long l)
-{
-    return i + c + l;
-}
+meta_table(weapon, const char *name; int damage; int weight; int munition) weapons {
+    meta_slot(WEAPON_PISTOL, "Pistol"; 2; 10; 200),
+    meta_slot(WEAPON_SHOTGUN,  "Shotgun"; 4; 20; 100),
+    meta_slot(WEAPON_PLASMA, "Plasma"; 5; 25; 250),
+    meta_slot(WEAPON_RAILGUN, "Railgun"; 10; 26; 100),
+    meta_slot(WEAPON_MAX, NULL; 0; 0; 0)
+};
+meta_introspect static int function(int i, char c, long l){return i + c + l;}
 
 #define META_IMPLEMENTATION
 #include "meta.h"
-
 int main(void)
 {
-
+    {   /* reflect */
+        int *i;
+        struct test t;
+        void *void_ptr = &t;
+        t.i = 5;
+        fprintf(stdout, "i: %d\n", t.i);
+        i = meta_member_ptr_from_name(void_ptr, "test", "i");
+        fprintf(stdout, "i: %d\n", *i);
+        *i = 10;
+        fprintf(stdout, "i: %d\n", *i);
+    }
+    {   /* enum value from string */
+        enum state s = meta_enum_value_from_string("state", "STATE_RUNNING");
+        fprintf(stdout, "STATE_RUNNING(%d) == %d\n", STATE_RUNNING, s);
+    }
+    {   /* String enum */
+        const char *star = meta_enum_str(punctuation, PUNCT_STAR);
+        fprintf(stdout, "PUNCT_STAR: %s\n", star);
+    }
+    {   /* Table */
+        const struct weapon *iter;
+        for (iter = meta_query(weapons, WEAPON_PISTOL); iter->name; iter++) {
+            fprintf(stdout, "%s:\n", iter->name);
+            fprintf(stdout, "index: %d\n", iter->index);
+            fprintf(stdout, "damage: %d\n", iter->damage);
+            fprintf(stdout, "weight: %d\n", iter->weight);
+            fprintf(stdout, "munition: %d\n", iter->munition);
+            fprintf(stdout, "\n");
+        }
+    }
     return 0;
 }
 
