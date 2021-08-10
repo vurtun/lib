61e11f543c2354ca290189ce32bc181069cd865b
vurtun <micha.mettke@gmail.com>
2021-08-10 19:44:27 +0200

    update benchmark

--- sinfl.h
+++ sinfl.h
@@ -33,10 +33,10 @@ this file implementation in *one* C or C++ file to prevent collisions.
 
 | Compressor name         | Compression| Decompress.| Compr. size | Ratio |
 | ------------------------| -----------| -----------| ----------- | ----- |
-| sdefl 1.0 -0            |   127 MB/s |   233 MB/s |    40004116 | 39.88 |
-| sdefl 1.0 -1            |   111 MB/s |   259 MB/s |    38940674 | 38.82 |
-| sdefl 1.0 -5            |    45 MB/s |   275 MB/s |    36577183 | 36.46 |
-| sdefl 1.0 -7            |    38 MB/s |   276 MB/s |    36523781 | 36.41 |
+| sdefl 1.0 -0            |   127 MB/s |   285 MB/s |    40004116 | 39.88 |
+| sdefl 1.0 -1            |   111 MB/s |   319 MB/s |    38940674 | 38.82 |
+| sdefl 1.0 -5            |    45 MB/s |   328 MB/s |    36577183 | 36.46 |
+| sdefl 1.0 -7            |    38 MB/s |   329 MB/s |    36523781 | 36.41 |
 | zlib 1.2.11 -1          |    72 MB/s |   307 MB/s |    42298774 | 42.30 |
 | zlib 1.2.11 -6          |    24 MB/s |   313 MB/s |    36548921 | 36.55 |
 | zlib 1.2.11 -9          |    20 MB/s |   314 MB/s |    36475792 | 36.48 |
